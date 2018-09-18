390a40d417b8a7d3b109691f0003f4d1c5c59d8c
Tomasz Łukaszyk <el.moue@gmail.com>
2018-09-18 17:20:35 +0200

    Add route to guest book

--- src/main/java/com/codecool/guest/book/App.java
+++ src/main/java/com/codecool/guest/book/App.java
@@ -8,7 +8,7 @@ import java.net.InetSocketAddress;
 public class App {
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
-        // TODO: set route
+        httpServer.createContext("/guest_book", new GuestBook());
         httpServer.setExecutor(null);
         httpServer.start();
     }
